a0c8abccd7219f7d3910803a6e4b896c69911975
Joaquín Araya <joa.arayam@duocuc.cl>
2026-09-13 19:48:00 -0300

    test: prueba de arranque del contexto de Spring

---
+++ src/test/java/cl/duoc/barriodigital/requests/RequestsApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.barriodigital.requests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
